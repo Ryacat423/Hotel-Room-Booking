57ecaca9a525d02e977f90f3c3bca82e3eb4d8f9
Ramil Yacat <ryacat56@gmail.com>
2025-09-20 08:51:07 +0800

    Added admin model

---
+++ models/admin.py
@@ -0,0 +1,15 @@
+from models.base_model import BaseModel
+import hashlib
+
+class Admin(BaseModel):
+    def get_table_name(self):
+        return 'admin'
+    
+    def authenticate(self, username, password):
+        hashed_password = hashlib.sha1(password.encode()).hexdigest()
+        result = self.select_where(['username', 'password'], (username, hashed_password))
+        return bool(result)
+    
+    def create_admin(self, username, password):
+        hashed_password = hashlib.sha1(password.encode()).hexdigest()
+        return self.insert(['username', 'password'], (username, hashed_password))
